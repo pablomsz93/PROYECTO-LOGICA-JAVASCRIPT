4f122500c93719162e7105d41529ab901cae1db6
“PABLO” <pablomanuel93@hotmail.com>
2024-05-06 12:16:43 +0200

    commit

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-export const fn = (/* argumentos de la función */) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
+export const fn = (empanadasA, empanadasB, empanadasC) => {
+   const precioEmpanadasA = 12;
+   const precioEmpanadasB = 14;
+   const precioEmpanadasC = 16;
+
+   const total = Math.ceil(((empanadasA * precioEmpanadasA) + (empanadasB * precioEmpanadasB) + (empanadasC * precioEmpanadasC)) / 3);
+
+if (empanadasA < 0 || empanadasB < 0 || empanadasC <0) {
+   throw Error
+}
+if ((empanadasA + empanadasB + empanadasC) % 3 !== 0) {
+   throw Error
+}
+if ((empanadasA + empanadasB + empanadasC) > 40){
+   throw Error
+}
+   return total;
 };
